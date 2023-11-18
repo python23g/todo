5e7afb4acd71351fcf87546e950f1b85fd506918
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-18 16:04:32 +0500

    models  todo

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.title}"
+    
+
+class Task(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128, unique=True)
+    description = models.TextField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
