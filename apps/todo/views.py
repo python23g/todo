728151769eb94399ed06a7b6e5c2a6c0eaa5c650
Mardon1207 <mardonsultonov8@gmail.com>
2023-11-23 09:53:57 +0500

    todo

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -42,9 +42,8 @@ class TodosItemView(View):
             user = User.objects.get(id=user_id)
         except User.DoesNotExist:
             return JsonResponse({'error': 'user not found.'})
-        todos = Todo.objects.filter(user=user)
         try:
-            todo = Todo.objects.get(id=todo_id)
+            todo = Todo.objects.get(id=todo_id,user=user)
         except User.DoesNotExist:
             return JsonResponse({'error': 'todo not found.'})
         result=model_to_dict(todo)
@@ -52,12 +51,11 @@ class TodosItemView(View):
     
     def put(self, request: HttpRequest, user_id: int, todo_id:int):
         try:
-            user = User.objects.get(id=user_id)
+            user = User.objects.get(id=user_id,user=user)
         except User.DoesNotExist:
             return JsonResponse({'error': 'user not found.'})
-        todos = Todo.objects.filter(user=user)
         try:
-            todo = Todo.objects.get(id=todo_id)
+            todo = Todo.objects.get(id=todo_id,user=user)
         except User.DoesNotExist:
             return JsonResponse({'error': 'todo not found.'})
         data=json.loads(request.body.decode())
@@ -72,9 +70,8 @@ class TodosItemView(View):
             user = User.objects.get(id=user_id)
         except User.DoesNotExist:
             return JsonResponse({'error': 'user not found.'})
-        todos = Todo.objects.filter(user=user)
         try:
-            todo = Todo.objects.get(id=todo_id)
+            todo = Todo.objects.get(id=todo_id,user=user)
         except User.DoesNotExist:
             return JsonResponse({'error': 'todo not found.'})
         todo.delete()
@@ -88,7 +85,7 @@ class TasksView(View):
         except User.DoesNotExist:
             return JsonResponse({'error': 'user not found.'})
         try:
-            todo = Todo.objects.get(id=todo_id)
+            todo = Todo.objects.get(id=todo_id,user=user)
         except User.DoesNotExist:
             return JsonResponse({'error': 'todo not found.'})
         tasks = Task.objects.filter(todo=todo)
@@ -104,7 +101,7 @@ class TasksView(View):
         except User.DoesNotExist:
             return JsonResponse({'error': 'user not found.'})
         try:
-            todo = Todo.objects.get(id=todo_id)
+            todo = Todo.objects.get(id=todo_id,user=user)
         except User.DoesNotExist:
             return JsonResponse({'error': 'todo not found.'})
         data= json.loads(request.body.decode())
@@ -120,19 +117,60 @@ class TasksView(View):
     
 
 class TasksItemView(View):
+
     def get(self, request: HttpRequest, user_id: int, todo_id: int, task_id: int) -> HttpRequest:
         try:
             user = User.objects.get(id=user_id)
         except User.DoesNotExist:
             return JsonResponse({'error': 'user not found.'})
         try:
-            todo = Todo.objects.get(id=todo_id)
+            todo = Todo.objects.get(id=todo_id,user=user)
         except Todo.DoesNotExist:
             return JsonResponse({'error': 'todo not found.'})
         try:
-            task = Task.objects.get(id=task_id)
+            task = Task.objects.get(id=task_id,todo=todo)
         except Task.DoesNotExist:
             return JsonResponse({'error': 'task not found.'})
         result=model_to_dict(task)
         return JsonResponse(result, safe=False)
     
+    def put(self, request: HttpRequest, user_id: int, todo_id:int, task_id:int):
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'user not found.'})
+        todos = Todo.objects.filter(user=user)
+        try:
+            todo = Todo.objects.get(id=todo_id,user=user)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'todo not found.'})
+        try:
+            task = Task.objects.get(id=task_id,todo=todo)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'task not found.'})
+        data=json.loads(request.body.decode())
+        task.todo=todo
+        task.title=data.get('title',task.title)
+        task.description=data.get('description',task.description)
+        task.status=data.get('status',task.status)
+        task.due_date=data.get('due_date',task.due_date)
+        task.save()
+        result=model_to_dict(todo)
+        return JsonResponse(result)
+    
+    def delete(self, request: HttpRequest, user_id: int, todo_id:int,task_id:int):
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'user not found.'})
+        todos = Todo.objects.filter(user=user)
+        try:
+            todo = Todo.objects.get(id=todo_id)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'todo not found.'})
+        try:
+            task = Task.objects.get(id=task_id,todo=todo)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'task not found.'})
+        task.delete()
+        return JsonResponse({"message":"delete"})
